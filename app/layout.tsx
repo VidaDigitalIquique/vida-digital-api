2f75dd753c1326e55f555064f1376e3c407f9eaf
VidaDigitalIquique <vidadigitaliquique@users.noreply.github.com>
2026-03-19 09:36:49 -0300

    feat: add VD favicon in blue

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'VidaDigital Inventario v2',
   description: 'Sistema de inventario para VidaDigital & Sanjh',
+  icons: {
+    icon: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({
